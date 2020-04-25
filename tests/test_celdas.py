87ec27af90b826d39829d247845bf8a34d7a15bb
Franco <fgozzerino@gmail.com>
2020-04-25 11:16:43 -0300

    Saque de test la funcion de columnas

--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -12,9 +12,3 @@ def test_no_menor_de_15():
        assert test_contar_celdas_ocupadas() > 14
 def test_no_mayor_de_15():
         assert test_contar_celdas_ocupadas() <16
-def test_columnas_no_vacias():
-    mi_carton = carton()
-    for i in range(8) :
-        if not( mi_carton[0][i] or mi_carton[1][i] or mi_carton[2][i] ):  
-                assert False
-    assert True 
