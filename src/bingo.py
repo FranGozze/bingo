b0b08203691766b37a935081784f28a3070c0f80
Franco <fgozzerino@gmail.com>
2020-06-04 17:24:58 -0300

    Agrego funcion que crea y checkea cartones

--- src/bingo.py
+++ src/bingo.py
@@ -1,13 +1,17 @@
-
+import math
+import random
 # Los 0 representan celdas vacias en el carton.
 # Los 1 representan celdas ocupadas en el carton.
-
+#   (0,11,0,32,44,0,62,73,0),
+ #       (8,0,25,38,0,56,0,0,80),
+  #      (0,17,29,0,47,0,67,0,88)
+    
 def carton():
-    carton = (
-        (0,11,0,32,44,0,62,73,0),
-        (8,0,25,38,0,56,0,0,80),
-        (0,17,29,0,47,0,67,0,88)
-    )
+    carton = [
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0]
+    ]
     return carton
 
 def contar_celdas_ocupadas(mi_carton):
@@ -152,3 +156,102 @@ def no_mas_de_2_celdas_vacias_juntas(mi_carton):
                 return False
   
     return True
+
+
+
+
+
+
+def intento_carton():
+    contador = 0
+    mi_carton = carton()
+    numerosCarton = 0
+
+    for numerosCarton in range(15):
+        contador += 1
+        if (contador == 50):
+            return intento_carton()
+
+        numero = random.randint(1, 90)
+
+        columna = math.floor (numero / 10)
+        if (columna == 9):
+             columna = 8
+        huecos = 0
+        for i in range(3) :
+            if mi_carton[i][columna] == 0:
+                huecos+=1
+            if (mi_carton[i][columna] == numero):
+                huecos = 0
+                break
+        if (huecos < 2):
+            continue
+        
+
+        fila = 0
+        for j in range(3):
+            huecos = 0
+            for i in range(9):
+                if (mi_carton[fila][i] == 0):
+                      huecos+=1
+                
+
+            if (huecos < 5 or mi_carton[fila][columna] != 0):
+                fila+=1
+            else:
+                break
+            
+    
+        if (fila == 3):
+            continue
+        
+
+        mi_carton[fila][columna] = numero
+        numerosCarton+=1
+        contador = 0
+        
+
+    for x in range(9):
+        huecos = 0
+        for y in range(3) :
+            if (mi_carton[y][x] == 0): 
+                 huecos+=1
+        
+        if (huecos == 3):
+            return intento_carton()
+        
+        
+
+    return mi_carton
+
+
+def imprimirCarton(mi_carton): 
+    print ("\n")
+    for fila in range (3): 
+        for columna in range(9):
+            print (mi_carton[fila][columna], sep= " ")
+        
+        print ("\n")
+    
+    print ("\n")
+
+
+
+
+while True:
+    mi_carton = intento_carton()
+    if(contar_celdas_ocupadas(mi_carton) == 15
+    and cinco_celdas_por_fila(mi_carton)
+    and filas_no_vacias(mi_carton)
+    and columnas_no_vacias(mi_carton)
+    and numeros_1_a_90(mi_carton)
+    and numeros_menores_al_siguiente(mi_carton)
+    and numeros_menores_arriba(mi_carton)
+    and numeros_no_repetidos(mi_carton)
+    and columnas_sin_tres_celdas(mi_carton)
+    and no_mas_de_2_celdas_juntas(mi_carton)
+    and no_mas_de_2_celdas_vacias_juntas(mi_carton)
+    and tres_columnas_una_celda(mi_carton) ):
+        print("Conseguido","\n",mi_carton[0],"\n",mi_carton[1],"\n",mi_carton[2])
+        
+        break
